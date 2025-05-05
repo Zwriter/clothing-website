7dc12c705a432023bf88efcc0350e35c723ac318
Zwriter <serbanoiurobert29@gmail.com>
2025-05-05 23:41:58 +0300

    finished authentification page

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,16 @@
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+
+import './authentification.styles.scss'
+
+const Authentication = () => {
+
+    return(
+        <div className='authentification-container'>
+            <SignInForm />
+            <SignUpForm />
+        </div>
+    )
+}
+
+export default Authentication;
